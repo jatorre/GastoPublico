83d1733eb8cce95826206255e1c60fcb71d6893c
Simon Tokumine <simon@japancentre.com>
2010-05-12 19:35:08 +0100

    added awesome nested set

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem "GeoRuby", :lib => "geo_ruby"
   config.gem "postgis_adapter"
+  config.gem "awesome_nested_set"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
